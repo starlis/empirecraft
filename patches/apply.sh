bf48e3be6ba79564e298666214befb79ee54cdab
Aikar <aikar@aikar.co>
2013-01-17 13:35:02 -0500

    Update apply to target HEAD instead of master

--- patches/apply.sh
+++ patches/apply.sh
@@ -7,13 +7,15 @@ function applyPatch {
     if [ ! -d "$basedir/EMC-$what" ]; then
         git clone github.com:aikar/EMC-$what EMC-$what
     fi
+    cd $basedir/$what
+    git branch -f upstream
     cd $basedir/EMC-$what
     echo "Synchronizing EMC-$what/master to $what/master"
-    git remote rm upstream
-    git remote add upstream ../$what
-    git co master
-    git fetch upstream
-    git reset --hard upstream/master
+    git remote rm upstream > /dev/null 2>&1
+    git remote add upstream ../$what >/dev/null 2>&1
+    git co master >/dev/null 2>&1
+    git fetch upstream >/dev/null 2>&1
+    git reset --hard upstream/upstream >/dev/null
     echo "  Applying patches to EMC-$what..."
     git am -3 $basedir/patches/$what/*.patch
     if [ "$?" != "0" ]; then
