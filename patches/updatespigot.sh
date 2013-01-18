4c40742caf96bd74c1c1af7a31f0b87948f9b4c7
Aikar <aikar@aikar.co>
2013-01-18 10:53:57 -0500

    Update spigot updater

--- patches/updatespigot.sh
+++ patches/updatespigot.sh
@@ -5,6 +5,7 @@ cd $basedir/EMC-CraftBukkit
 git co spigot
 git fetch spigot
 git reset --hard spigot/master
-git reset --soft upstream/master
+git fetch upstream
+git reset --soft upstream/upstream
 git commit -a --author="md_5 <md_5@bigpond.com>" -m "Spigot"
-git format-patch --stdout HEAD^ > ../patches/CraftBukkit/0001*.patch
+git format-patch --stdout HEAD^ > ../patches/CraftBukkit/0001-Spigot.patch
