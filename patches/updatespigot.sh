0226e4ee3e62b6fc18c359fb65e3bd0811e92709
Aikar <aikar@aikar.co>
2013-03-02 17:30:29 -0500

    Update bukkit for spigot too

--- patches/updatespigot.sh
+++ patches/updatespigot.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 basedir=$(dirname $(readlink -f $0))
 basedir=$(dirname $basedir)
-cd $basedir/CraftBukkit
-git branch -f upstream
-cd $basedir/EMC-CraftBukkit
-git co spigot
-git fetch spigot
-git reset --hard spigot/master
-git fetch upstream
-git reset --soft upstream/upstream
-git commit -a --author="md_5 <md_5@bigpond.com>" -m "Spigot"
-git format-patch --stdout HEAD^ > ../patches/CraftBukkit/0001-Spigot.patch
+function update {
+    target=$1
+    cd $basedir/$target
+    git branch -f upstream
+    cd $basedir/EMC-$target
+    git co spigot
+    git fetch spigot
+    git reset --hard spigot/master
+    git fetch upstream
+    git reset --soft upstream/upstream
+    git commit -a --author="md_5 <md_5@bigpond.com>" -m "Spigot"
+    git format-patch --stdout HEAD^ > ../patches/$target/0001-Spigot.patch
+    git co master
+}
+
+update Bukkit
+update CraftBukkit
