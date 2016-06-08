09bdc7e81a306ecb696e4993efe4b4fc03ca2778
Aikar <aikar@aikar.co>
2016-06-07 23:53:09 -0400

    rename bat to sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,24 @@
-compile.bat
+#!/usr/bin/env bash
+
+if [ ! -d "EmpireCraft-API" ]; then
+git clone git@bitbucket.org:starlis/EmpireCraft-API
+fi
+
+if [ ! -d "EmpireCraft-Server" ]; then
+git clone git@bitbucket.org:starlis/EmpireCraft-Server
+fi
+
+cd EmpireCraft-API
+git fetch origin
+git reset --hard origin/master
+
+cd ..
+
+cd EmpireCraft-Server
+git fetch origin
+git reset --hard origin/master
+
+cd ..
+if [ "$1" != "--nocompile" ]; then
+	mvn clean install
+fi
