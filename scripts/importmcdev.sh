f2a9cd3d3c5ddd283e5dcdad2014ac59a5279499
Aikar <aikar@aikar.co>
2017-09-28 21:38:45 -0400

    Update to 1.12.2

--- scripts/importmcdev.sh
+++ scripts/importmcdev.sh
@@ -10,7 +10,7 @@ done
 
 workdir=$basedir/Paper/work
 minecraftversion=$(cat $basedir/Paper/work/BuildData/info.json | grep minecraftVersion | cut -d '"' -f 4)
-decompiledir=$workdir/$minecraftversion
+decompiledir=$workdir/Minecraft/$minecraftversion
 
 nms="net/minecraft/server"
 export MODLOG=""
