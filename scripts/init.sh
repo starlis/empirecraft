755bbeb651a871a8f2a9cd725301ae7588ddeafc
Aikar <aikar@aikar.co>
2018-10-19 00:19:51 -0400

    rebuild all patches to be taco'ized

--- scripts/init.sh
+++ scripts/init.sh
@@ -26,18 +26,21 @@ function bashColorReset {
 function cleanupPatches {
 	cd "$1"
 	for patch in *.patch; do
+		sed -ri 's/index [a-f0-9]+\.\.[a-f0-9]+/index 7ac07ac07ac0..7ac07ac07ac0/g' "$patch"
+		sed -ri 's/^From [a-f0-9]{32,}/From 7ac07ac07ac07ac07ac07ac07ac07ac07ac07ac0/g' "$patch"
+		git add -A $patch
 		gitver=$(tail -n 2 $patch | grep -ve "^$" | tail -n 1)
-		diffs=$(git diff --staged $patch | grep -E "^(\+|\-)" | grep -Ev "(From [a-z0-9]{32,}|\-\-\- a|\+\+\+ b|.index|Date\: )")
+		diffs=$(git diff --staged $patch | grep -E "^(\+|\-)" | grep -Ev "(From [a-f0-9]{32,}|\-\-\- a|\+\+\+ b|.index|Date\: )")
 
 		testver=$(echo "$diffs" | tail -n 2 | grep -ve "^$" | tail -n 1 | grep "$gitver")
 		if [ "x$testver" != "x" ]; then
 			diffs=$(echo "$diffs" | tail -n +3)
 		fi
 
-		if [ "x$diffs" == "x" ] ; then
-			git reset HEAD $patch >/dev/null
-			git checkout -- $patch >/dev/null
-		fi
+#		if [ "x$diffs" == "x" ] ; then
+			#git reset HEAD $patch >/dev/null
+			#git checkout -- $patch >/dev/null
+#		fi
 	done
 }
 function pushRepo {
