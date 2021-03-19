1b579407fc58913b483d42da15686e77b13b50d0
chickeneer <emcchickeneer@gmail.com>
2021-03-18 19:12:46 -0500

    Improve importmcdev script and fix first server patch

--- scripts/importmcdev.sh
+++ scripts/importmcdev.sh
@@ -18,22 +18,18 @@ cd $basedir
 
 export importedmcdev=""
 function import {
-	if [ -f "$basedir/Paper/Paper-Server/src/main/java/net/minecraft/server/$1.java" ]; then
-		echo "ALREADY IMPORTED $1"
-		return 0
-	fi
-	export importedmcdev="$importedmcdev $1"
+  export importedmcdev="$importedmcdev $1"
 	file="${1}.java"
 	target="$basedir/Paper/Paper-Server/src/main/java/$nms/$file"
 	base="$decompiledir/$nms/$file"
 
-	if [[ ! -f "$target" ]]; then
-		export MODLOG="$MODLOG  Imported $file from mc-dev\n";
+	if [ ! -f "$target" ]; then
+	  export MODLOG="$MODLOG  Imported $file from mc-dev\n";
 		echo "$(bashColor 1 32) Copying $(bashColor 1 34)$base $(bashColor 1 32)to$(bashColor 1 34) $target $(bashColorReset)"
 		mkdir -p "$(dirname "$target")"
-		cp "$base" "$target"
+		cp "$base" "$target" || exit 1
 	else
-		echo "$(bashColor 1 33) UN-NEEDED IMPORT STATEMENT:$(bashColor 1 34) $file $(bashColorReset)"
+	  echo "$(bashColor 1 33) UN-NEEDED IMPORT STATEMENT:$(bashColor 1 34) $file $(bashColorReset)"
 	fi
 }
 
@@ -65,9 +61,9 @@ function importLibrary {
 	fi
 )
 
-files=$(cat "$basedir/server/"* | grep "+++ b/src/main/java/net/minecraft/" | sort | uniq | sed 's/\+\+\+ b\/src\/main\/java\/net\/minecraft\///g')
+files=$(cat "$basedir/patches/server/"* | grep "+++ b/src/main/java/net/minecraft/" | sort | uniq | sed 's/\+\+\+ b\/src\/main\/java\/net\/minecraft\///g')
 
-nonnms=$(cat patches/server/* | grep "create mode " | grep -Po "src/main/java/net/minecraft/(.*?).java" | sort | uniq | sed 's/src\/main\/java\/net\/minecraft\///g' | sed 's/.java//g')
+nonnms=$(grep -R "new file mode" -B 1 "$basedir/patches/server/" | grep -v "new file mode" | grep -oE --color=none "net\/minecraft\/.*.java" | sed 's/.*\/net\/minecraft\///g')
 function containsElement {
 	local e
 	for e in "${@:2}"; do
@@ -78,7 +74,7 @@ function containsElement {
 for f in $files; do
 	containsElement "$f" ${nonnms[@]}
 	if [ "$?" == "1" ]; then
-		if [ ! -f "$basedir/Paper/Paper-Server/src/main/java/net/minecraft/$f.java" ]; then
+		if [ ! -f "$basedir/Paper/Paper-Server/src/main/java/net/minecraft/$f" ]; then
 		f="$(echo "$f" | sed 's/.java//g')"
 			if [ ! -f "$decompiledir/$nms/$f.java" ]; then
 				echo "$(bashColor 1 31) ERROR!!! Missing NMS$(bashColor 1 34) $f $(bashColorReset)";
@@ -116,7 +112,7 @@ fi
 ########################################################
 ########################################################
 (
-	cd Paper/Paper-Server/
+	cd $basedir/Paper/Paper-Server/
 	rm -rf nms-patches
 	git add src -A
 	echo -e "EMC-Extra mc-dev Imports\n\n$MODLOG" | git commit src -F -
